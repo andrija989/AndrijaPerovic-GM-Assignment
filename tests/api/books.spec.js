3ef34bada9e050f5baf395512318dbf081a1234c
andrija989 <andrija989@gmail.com>
2023-07-06 08:34:38 +0200

    negative api tests

--- tests/api/books.spec.js
+++ tests/api/books.spec.js
@@ -26,9 +26,25 @@ test.describe("API scenarios - books", () => {
     await booksAPI.post({ userData, collectionOfIsbns: allBooks });
   });
 
+  test("Post Book to user profile - negative", async ({ booksAPI }) => {
+    await booksAPI.post({
+      userData,
+      collectionOfIsbns: [{ isbn: "invalidISBN" }],
+      statusCode: 400,
+    });
+  });
+
   test("Delete all Books from user profile", async ({ booksAPI }) => {
     for await (const book of allBooks) {
       await booksAPI.delete({ userData, isbn: book.isbn });
+    }
+  });
+
+  test("Delete all Books from user profile - negative, try to delete books that are not in the inventory", async ({
+    booksAPI,
+  }) => {
+    for await (const book of allBooks) {
+      await booksAPI.delete({ userData, isbn: book.isbn, statusCode: 400 });
     }
   });
 });
